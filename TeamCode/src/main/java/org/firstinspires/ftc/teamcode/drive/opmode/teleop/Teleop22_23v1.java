d3e35a9cd490bdd7516159489fa3f6e0b4f98f47
violet-flowers <109183645+violet-flowers@users.noreply.github.com>
2022-10-12 10:08:30 -0500

    remove wall distance code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/Teleop22_23v1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/Teleop22_23v1.java
@@ -15,8 +15,6 @@ public class Teleop22_23v1 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        DistanceSensor distance;
-        distance = hardwareMap.get(DistanceSensor.class, "frontDist");
 
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
@@ -46,54 +44,18 @@ public class Teleop22_23v1 extends LinearOpMode {
                 );
             }
 
-            if (gamepad1.x) {
-                drive.turn(Math.toRadians(180));
-            }
-            if (gamepad1.dpad_up && fromWall < 12) {
-                fromWall += 1;
-                sleep(200);
-            }
-            if (gamepad1.dpad_down && fromWall > 0) {
-                fromWall -= 1;
-                sleep(200);
-            }
+          
+
             drive.update();
 
             Pose2d poseEstimate = drive.getPoseEstimate();
             telemetry.addData("x", poseEstimate.getX());
             telemetry.addData("y", poseEstimate.getY());
             telemetry.addData("heading", poseEstimate.getHeading());
-            telemetry.addData("Distance", distance.getDistance(DistanceUnit.INCH));
-            telemetry.addData("fromWall", fromWall);
             telemetry.update();
 
-            if (gamepad1.a) {
-                double dist = distance.getDistance(DistanceUnit.INCH);
-
-                /*
-                method 1 uses the pose estimate, you can increase or decrease "fromWall"
-                 to drive that distance from the wall (assuming the wall is x = 0)
-                dpad up/down can be used to change the "fromWall" distance in a match
-                */
-
-                TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
-                        // .forward(poseEstimate.getX() - fromWall)
-                        .strafeLeft(poseEstimate.getX() - fromWall)
-                        //.turn(Math.toRadians(180))
-                        .build();
-
-                /*
-                method 2 works the same but uses the distance sensor
-                */
-//                TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
-//                        .forward(dist - fromWall)
-//                        //.turn(Math.toRadians(180))
-//                        .build();
 
 
-                drive.followTrajectorySequence(trajSeq);
-
-            }
         }
     }
 
