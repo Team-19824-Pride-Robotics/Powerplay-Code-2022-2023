3e21da5cf8f0f63a7737ece7ddccedacf914600e
Yajie Li <104532860+YoshiVox1@users.noreply.github.com>
2022-11-11 22:11:34 -0600

    Update a_3_alaN_Auto_Final_Right.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/a_3_alaN_Auto_Final_Right.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/a_3_alaN_Auto_Final_Right.java
@@ -319,7 +319,7 @@ public class a_3_alaN_Auto_Final_Right extends LinearOpMode {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 2");
 
         //  Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
